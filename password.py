7137c874f995f6d7686e2b4b65acfdbe322e9bc8
parham taki <parhamtaki85@gmail.com>
2022-11-25 23:19:36 +0330

    Update password.py
    make it faster

--- password.py
+++ password.py
@@ -1,19 +1,9 @@
 from string import ascii_lowercase, digits, ascii_uppercase
-from typing import Iterable
 import random
 
 
-def random_str(length: int = None, *, chars: Iterable = frozenset((ascii_lowercase, digits, ascii_uppercase)), r: random.Random = random) -> str:
-    length = length or r.randint(20, 30)
-    out = []
-    all_ = ""
-    for x in chars:
-        out.append(r.choice(x))
-        all_ += x
-        length -= 1
-    out.extend(r.choices(all_, k=length))
-    r.shuffle(out)
-    return "".join(out)
+def random_str(length: int = None, *, chars: str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-.+_", r: random.Random = random) -> str:
+    return "".join(r.choices(chars, k=length or r.randint(20, 30)))
 
 
 if __name__ == "__main__":
